3cf45e040a619a6e4dd42a5531478793d3d15aca
Trey Keown <jfktrey@gmail.com>
2016-12-27 03:42:59 -0600

    Initial commit with test file.

---
+++ cmake-build-debug/CMakeFiles/ornament.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ornament.dir/src/test.cpp.o"
+  "ornament.pdb"
+  "ornament"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ornament.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
